374b09386538c1656c69e703ec668fd408f9b3c3
Piotr Szeląg <szelares@gmail.com>
2017-09-19 19:22:57 +0200

    Create adjacentElementsProduct.js

---
+++ adjacentElementsProduct.js
@@ -0,0 +1,6 @@
+function adjacentElementsProduct(inputArray) {
+   let product = inputArray[0]*inputArray[1];
+    for(var i = 0; i < inputArray.length; i++) {
+     product = inputArray[i]*inputArray[i+1] > product ? inputArray[i]*inputArray[i+1] : product;
+    } return product;
+}   
